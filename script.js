de270e10404a4a8188a7dc0618e04c96d94e75b2
Obayad Hasan Konok <118033521+obayad@users.noreply.github.com>
2024-11-20 00:26:43 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+alert ("Welcome to Nazmul's Portfolio")
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Message sent! Thank you for reaching out.');
+  });
